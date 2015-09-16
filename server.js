2e1421c78211b5a4914fcde22354e7ad34d0f850
Marius <maryus_blondu09@yahoo.com>
2015-09-16 17:28:39 +0200

    aaaaaaa

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+const crypto = require('crypto'),
+    fs = require("fs"),
+    http = require("http");
+
+var privateKey = fs.readFileSync('privatekey.pem').toString();
+var certificate = fs.readFileSync('certificate.pem').toString();
+
+var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
+
+var handler = function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+};
+
+var server = http.createServer();
+server.setSecure(credentials);
+server.addListener("request", handler);
+server.listen(8080);
