464b35c018ab904e7c75c841b438597229cbc044
Marius <maryus_blondu09@yahoo.com>
2015-10-07 14:50:09 +0200

    Function

--- public/javascripts/Function.js
+++ public/javascripts/Function.js
@@ -1,15 +1,9 @@
 var canvas = document.getElementById('canvas-1');
 console.log("canvas : " + canvas);
 var SNAKE_RADIOUS = 15;
-var SNAKE_LENGHT = 10;
+var SNAKE_LENGHT = 15;
 var SNAKE_CAP=20;
 
-
-//var path = new Path.Circle({
-//  center: [20,20],
-//  radius: 20,
-//  fillColor: 'red'
-//});
 var snake = [];
 var cap = new Path.Circle({
   center: [20,20],
@@ -17,6 +11,7 @@ var cap = new Path.Circle({
   fillColor: 'red'
 });
 snake.push(cap);
+
 for(var i=1;i<SNAKE_LENGHT;i++){
   var segment = new Path.Circle({
     center: [20,20+20*i],
@@ -38,8 +33,9 @@ function onFrame(event) {
   var speed = 100;
     var vector = pos - snake[0].position;
     snake[0].position += vector / speed;
-    for (var  i = SNAKE_LENGHT-1;i>0; i--) {
-      var positionaux=snake[i].position;
-      snake[i].position =(positionaux+snake[i-1].position)/2;
+    for (var i = SNAKE_LENGHT - 1; i > 0; i--) {
+      var positionaux = snake[i].position;
+      snake[i].position = (positionaux + positionaux + snake[i - 1].position) / 3;
     }
-  }
+
+}
