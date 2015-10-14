bfd0d34a9e9d595e0df8938ca4b8755b77792343
Marius <maryus_blondu09@yahoo.com>
2015-10-14 16:15:25 +0200

    14.10

--- public/javascripts/Function.js
+++ public/javascripts/Function.js
@@ -6,7 +6,7 @@ var SNAKE_CAP=20;
 
 var snake = [];
 var cap = new Path.Circle({
-  center: [20,20],
+  center: [400,400],
   radius: SNAKE_CAP,
   fillColor: 'red'
 });
@@ -14,23 +14,34 @@ snake.push(cap);
 
 for(var i=1;i<SNAKE_LENGHT;i++){
   var segment = new Path.Circle({
-    center: [20,20+20*i],
+    center: [400,400+20*i],
     radius: SNAKE_RADIOUS,
     fillColor: 'red'
   });
   snake.push(segment);
 }
 
+var ws = new WebSocket('wss://localhost:8000/');
+//ws.on('open', function() {
+//  ws.send('something');
+//});
+ws.onmessage = function(message) {
+  console.log('received: %s', message.data);
+  var obj = JSON.parse(message.data);
+  pos.x = obj.x;
+  pos.y = obj.y;
+};
 
-var pos = new Point(20,20);
+var pos = new Point(400,400);
+
+
 
 function onMouseDown(event){
   pos = event.point;
 }
 
 function onFrame(event) {
-  console.log(event.count);
-  var speed = 100;
+    var speed = 100;
     var vector = pos - snake[0].position;
     snake[0].position += vector / speed;
     for (var i = SNAKE_LENGHT - 1; i > 0; i--) {
